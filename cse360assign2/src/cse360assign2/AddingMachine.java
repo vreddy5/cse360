5c709a7006616dd43f7217babbb6c118612e52d9
Vikas Reddy <vikasreddy6789@gmail.com>
2019-10-09 20:02:15 -0700

    First Commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,37 @@
+package cse360assign2;
+
+//performs simple arithmetic functions
+public class AddingMachine {
+
+	private int total;
+
+	//constructor
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//gets the current total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//value is passed in to be added to total
+	public void add (int value) {
+		
+	}
+	
+	//value is passed in to be subtracted from total
+	public void subtract (int value) {
+		
+	}
+	
+	//returns a human-readable string
+	public String toString () {
+		return "";
+	}
+	
+	//clears the total field
+	public void clear() {
+	
+	}
+}
